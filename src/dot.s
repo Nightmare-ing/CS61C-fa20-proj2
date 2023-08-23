d22f3c7d7ecdba8a67c8b7687bcc3e332a878542
Qingsong Hu <kingsanhu@gmail.com>
2023-08-23 13:07:08 +0800

    Finish Task3.1: Dot Product

--- src/dot.s
+++ src/dot.s
@@ -18,27 +18,45 @@
 #   this function terminates the program with error code 76.
 # =======================================================
 dot:
-
-    # Prologue
-
+    blt x0, a2, check_strides
+    li a0, 17
+    li a1, 75
+    ecall
+check_strides:
+    slt t0, x0, a3
+    slt t1, x0, a4
+    and t0, t0, t1
+    bne x0, t0, loop_start
+    li a0, 17
+    li a1, 76
+    ecall
 
 loop_start:
-
-
-
-
-
-
-
-
-
-
-
+    # Prologue
+    addi sp, sp, -4
+    sw s0 0(sp)
+    
+    li t0, 0 # the counter
+    li s0, 0
+loop_continue:
+    bge t0, a2, loop_end
+    mul t1, t0, a3
+    mul t2, t0, a4
+    slli t1, t1, 2
+    slli t2, t2, 2
+    add t1, a0, t1
+    add t2, a1, t2
+    lw t1, 0(t1)
+    lw t2, 0(t2)
+    mul t1, t1, t2
+    add s0, s0, t1
+    addi t0, t0, 1
+    j loop_continue
 
 loop_end:
-
-
+    mv a0, s0
     # Epilogue
-
+    lw s0, 0(sp)
+    addi sp, sp, 4
     
     ret
